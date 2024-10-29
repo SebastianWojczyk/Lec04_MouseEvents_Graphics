3bf64907acc55347f4510c9869c009d45352793a
SebastianWojczyk <62283873+SebastianWojczyk@users.noreply.github.com>
2024-10-29 11:44:46 +0100

    Add files via upload

---
+++ 08_MouseEvents_Graphics/08_MouseEvents_Graphics/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace _08_MouseEvents_Graphics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxImage = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownPenWidth = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonPenColor = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPenWidth)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxImage
+            // 
+            this.pictureBoxImage.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBoxImage.Location = new System.Drawing.Point(153, 27);
+            this.pictureBoxImage.Name = "pictureBoxImage";
+            this.pictureBoxImage.Size = new System.Drawing.Size(635, 411);
+            this.pictureBoxImage.TabIndex = 0;
+            this.pictureBoxImage.TabStop = false;
+            this.pictureBoxImage.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxImage_MouseDown);
+            this.pictureBoxImage.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBoxImage_MouseMove);
+            this.pictureBoxImage.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBoxImage_MouseUp);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.numericUpDownPenWidth);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.buttonPenColor);
+            this.groupBox1.Location = new System.Drawing.Point(13, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(134, 411);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tools";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pen width";
+            // 
+            // numericUpDownPenWidth
+            // 
+            this.numericUpDownPenWidth.Location = new System.Drawing.Point(66, 49);
+            this.numericUpDownPenWidth.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.numericUpDownPenWidth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownPenWidth.Name = "numericUpDownPenWidth";
+            this.numericUpDownPenWidth.Size = new System.Drawing.Size(62, 20);
+            this.numericUpDownPenWidth.TabIndex = 2;
+            this.numericUpDownPenWidth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownPenWidth.ValueChanged += new System.EventHandler(this.numericUpDownPenWidth_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pen color";
+            // 
+            // buttonPenColor
+            // 
+            this.buttonPenColor.BackColor = System.Drawing.Color.Black;
+            this.buttonPenColor.Location = new System.Drawing.Point(64, 19);
+            this.buttonPenColor.Name = "buttonPenColor";
+            this.buttonPenColor.Size = new System.Drawing.Size(64, 23);
+            this.buttonPenColor.TabIndex = 0;
+            this.buttonPenColor.UseVisualStyleBackColor = false;
+            this.buttonPenColor.Click += new System.EventHandler(this.buttonPenColor_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save as ...";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBoxImage);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPenWidth)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxImage;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonPenColor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDownPenWidth;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+    }
+}
+
---
+++ 08_MouseEvents_Graphics/08_MouseEvents_Graphics/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _08_MouseEvents_Graphics
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        private Pen pen;
+        private Point pointTMP;
+        private const string filters = "Bitmap|*.bmp|PNG File|*.png|Jpeg|*.jpg";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            newToolStripMenuItem_Click(null, null);
+
+            pen = new Pen(Color.Black);
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            pointTMP = Point.Empty;
+        }
+
+        private void pictureBoxImage_MouseMove(object sender, MouseEventArgs e)
+        {   this.Text = e.Location.ToString() + " " + e.Button.ToString();
+            if(e.Button == MouseButtons.Left)
+            {
+                //graphics.DrawEllipse(pen, e.X, e.Y, 10, 10);
+                graphics.DrawLine(pen, pointTMP, e.Location);
+                pointTMP = e.Location;
+            }
+            pictureBoxImage.Refresh();
+        }
+        private void pictureBoxImage_MouseDown(object sender, MouseEventArgs e)
+        {   this.Text = e.Location.ToString() + " " + e.Button.ToString();
+            if (e.Button == MouseButtons.Left)
+            {
+                pointTMP = e.Location;
+            }
+        }
+        private void pictureBoxImage_MouseUp(object sender, MouseEventArgs e)
+        {   this.Text = e.Location.ToString() + " " + e.Button.ToString();
+            if (e.Button == MouseButtons.Left)
+            {
+                pointTMP = Point.Empty;
+            }
+        }
+
+        private void buttonPenColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+
+            cd.Color = buttonPenColor.BackColor;
+            if(cd.ShowDialog() == DialogResult.OK)
+            {
+                buttonPenColor.BackColor = cd.Color;
+                pen.Color = cd.Color;
+            }
+        }
+
+        private void numericUpDownPenWidth_ValueChanged(object sender, EventArgs e)
+        {
+            pen.Width = (float)numericUpDownPenWidth.Value;
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBoxImage.Image = new Bitmap(pictureBoxImage.Width, pictureBoxImage.Height);
+            graphics = Graphics.FromImage(pictureBoxImage.Image);
+            graphics.Clear(Color.White);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = filters;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBoxImage.Image = new Bitmap(ofd.FileName);
+                graphics = Graphics.FromImage(pictureBoxImage.Image);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.Filter = filters;
+            if(sfd.ShowDialog() == DialogResult.OK)
+            {
+                if (sfd.FileName.EndsWith("bmp"))
+                {
+                    pictureBoxImage.Image.Save(sfd.FileName, ImageFormat.Bmp);
+                }
+                else if (sfd.FileName.EndsWith("png"))
+                {
+                    pictureBoxImage.Image.Save(sfd.FileName, ImageFormat.Png);
+                }
+                else if (sfd.FileName.EndsWith("jpg"))
+                {
+                    pictureBoxImage.Image.Save(sfd.FileName, ImageFormat.Jpeg);
+                }
+            }
+        }
+    }
+}
